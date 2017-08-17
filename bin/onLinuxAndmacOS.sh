d0263509765ce18e3c0b26b12e211e0826564fb3
solo7773 <zhnanx@gmail.com>
2017-08-17 13:58:53 +1000

    Update onLinuxAndmacOS.sh

--- bin/onLinuxAndmacOS.sh
+++ bin/onLinuxAndmacOS.sh
@@ -1,1 +1,11 @@
-ttt
+echo "Your working directory is"
+echo ""
+pwd
+echo ""
+echo "Beware this is where the program will save temporary data to."
+
+echo -n "When you are ready, press [ENTER] to proceed"
+read nouse_var
+echo ""
+
+~/anaconda3/bin/python ../ncgui.py
