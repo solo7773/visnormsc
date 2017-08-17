cc985c8736867a3b25de5d7c0f7aa30d10e40c97
solo7773 <zhnanx@gmail.com>
2017-08-17 15:19:48 +1000

    Update onLinuxAndmacOS.sh

--- bin/onLinuxAndmacOS.sh
+++ bin/onLinuxAndmacOS.sh
@@ -3,9 +3,13 @@ echo ""
 pwd
 echo ""
 echo "Beware this is where the program will save temporary data to."
-
+echo ""
 echo -n "When you are ready, press [ENTER] to proceed"
 read nouse_var
 echo ""
 
-~/anaconda3/bin/python ../ncgui.py
+if ~/anaconda3/bin/python ../ncgui.py; then
+	echo "Everything is ok. Quit program."
+else
+	echo "python cannot be found, please make sure you have installed python correctly and the path to python has been added to the environment variable PATH."
+fi
